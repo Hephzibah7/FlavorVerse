8be670d25c4dbc3499835b6229741671af885a2e
Ayash <ayashbehera2003@gmail.com>
2023-10-14 14:27:55 +0530

    changes

--- recipefinder/src/components/homepage/components/CustomImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CustomImage({imgSrc, pt}) {
-  return (
-    <div className='custom-image' style={{paddingTop: pt}}>
-      <img src={imgSrc} alt="" />
-    </div>
-  )
-}
